dacc2ae5f4360d7d9823729d1df36c683f34c73e
Wiktor Chumakov <Apostal333@gmail.com>
2025-11-17 22:08:24 +0300

    modified Tavern

--- src/main/kotlin/nyetHack/Tavern.kt
+++ src/main/kotlin/nyetHack/Tavern.kt
@@ -1,5 +1,7 @@
 package nyetHack
 
+const val TAVERN_NAME = "Taernyl's Folly"
+
 data class Drink(
     val beer: String = "beer",
     val sprat: String = "sprat",
@@ -30,19 +32,52 @@ fun main() {
 //            else -> "not drink"
 //        }
 //    )
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) { // проверяет строку на символы, isNotEmpty() проверяет длину строки
-            it.lowercase()
-        } else {
-            "is not drink"
-        }
-    }
-
-    var num = 0
-    while (num < 3) {
-        println(Drink.fromInput(readlnOrNull()))
-        num++
-    }
+//    var beverage = readLine()?.let {
+//        if (it.isNotBlank()) { // проверяет строку на символы, isNotEmpty() проверяет длину строки
+//            it.lowercase()
+//        } else {
+//            "is not drink"
+//        }
+//    }
+//
+//    var num = 0
+//    while (num < 3) {
+//        println(Drink.fromInput(readlnOrNull()))
+//        num++
+//    }
 
     //println(Drink.fromInput(readlnOrNull()))
+    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
+
 }
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    /*Так как split() возвращает список, для нее поддерживается упрощенный синтаксис,
+    который называется деструктуризацией. Деструктуризация - это возможность объявить
+    и инициализировать сразу несколько переменных*/
+    val (type, name, price) = menuData.split(',').map { it.trim() }
+    println("Madrigal buys a $name ($type) for $price.")
+    val phrase =if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${("Ah, delicious $name!").toDragonSpeak()}"
+    } else{
+        "Madrigal says: Thanks for the $name."
+    }
+    println(phrase)
+}
+
+private fun String.toDragonSpeak() =
+    replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
